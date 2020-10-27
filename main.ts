cdbdf00f41bd129c218c1823cd1f477ec588eaf6
RobotNeko <73191153+RobotNeko@users.noreply.github.com>
2020-10-27 14:14:42 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Square)
+})
